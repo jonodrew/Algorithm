42dc889cb93150a6dd970f49e362df4e0e4da4f5
Jonathan Kerr <jonathandrewkerr@gmail.com>
2016-10-28 14:39:56 +0100

    MVP

--- tix_allocate.py
+++ tix_allocate.py
@@ -3,126 +3,6 @@ import numpy as np
 from collections import Counter
 import random
 
-"""
-read csv
-read ratios
-analysis: what proportions do we have?
-Random select?
-Random select according to ratios
-
-OR
-
-Select according to day
-apply ratios to results
-risks small number in one and unsuitable ratios
-hence: ratio global or daily?
-Global ratio allows for all OOL on one day
-
-x total df2
-y OOL, z London
-a CSR, b non-CSR
-    a   b
-y   +   +
-z   +   +
-
-Reduce to stream in ratio 35:10:5 for sum of 800
-random selection of region in appropriate ratios
-then sort according to preferred day and count
-
-Throw 800 people in at random
-Switch out two
-If better: keep
-If worse: switch back
-repeat for 10^x turns
-metrics:
-stream ratio (overall)
-while CSR = 70% and non-CSR = 20% and FT = 10%:
-    switch people in and out and as long as above stay within tolerances:
-        when in scope, move on to day such that:
-            preferred day = +2 and optimise for that day's score
-    region ratio (by day? Overall?)
-got preferred day? (overall)
-"""
-
-"""
-create list of dictionaries. Dict. key is delegate code (for anonymity) and
-values are stream, region, day
-calculate stream metrics
-if stream_valid = True:
-    calculate region metrics
-    if region_valid = True:
-        for 1000 turns:
-            calcuate day metrics
-            swap delegate with low day score
-            if region_valid
-            calculate day metrics
-            if day metrics better:
-                keep df1
-            if day metrics the same:
-                flip a coin
-            else:
-                swap back
-
-    else:
-        swap a delegate from over-subscribed region
-        calculate stream metrics
-        if stream_valid = False:
-            break
-        else:
-            calculate region metrics
-
-
-def calculate(columnName, dict.):
-    for key in dict:
-        count instances of key/len(columnName) for % instances in frame
-
-def streamCalc(CSR_metric, NCSR_metric, FT_metric):
-    if CSR_metric > 0.68 and CSR_metric < 0.72:
-        CSR_valid = True
-
-    if NCSR_metric > 0.18 and NCSR_metric < 0.22:
-        NCSR_valid = True
-
-    if FT_metric > 0.08 and FT_metric < 0.12:
-        FT_valid = True
-
-    if CSR_metric + NCSR_metric + FT_metric = 1.0:
-        return stream_valid = True
-
-
-df1 = file.open(heresmyfile.csv)
-
-def __main__(arg):
-    generate empty dataframe: index applicant number, columns: stream, region, day
-    randomly assign 800 df1 to it
-    streamLoop = 0
-    regionLoop = 0
-    dayLoop = 0
-    while iterations <= 1000:
-        count up number of stream and calculate %
-        if within tolerances:
-            while within tolerances:
-                regionLoop += 1
-                count up according to region and calculate %
-                if within tolerances:
-                    while within tolerances:
-                        dayLoop += 1
-                        count up score for days where 2 = perfect, 1 = preferred, 0 = impossible
-                        if score = len(list)*2:
-                            break and go to the pub
-                        else:
-                            swap delegate in list with list of df2
-                else:
-                    swap delegate in list with list of df2
-        else:
-            swap delegate in list with list of df2
-
-
-
-while not stream_valid:
-    #code goes in here
-    pass
-"""
 def swapFunc(df1, df2):
     df1 = reindex(df1) #to make sure the random number is in the index
     df2 = reindex(df2) #ditto
@@ -134,9 +14,9 @@ def swapFunc(df1, df2):
     df2 = df2.drop(rand2) #deletes that row from df2
     return df1,df2
 
-def dayCalc(df1):
-    day1 = df1['Day 1'].value_counts()
-    day2 = df1['Day 2'].value_counts()
+def dayCalc(df):
+    day1 = df['Day 1'].value_counts()
+    day2 = df['Day 2'].value_counts()
     if day1.ix[1] > 250 and day2.ix[1] > 250:
         dayMetric = True
     else:
@@ -145,8 +25,9 @@ def dayCalc(df1):
 
 def regionCalc(df):
     regionDict = dict(Counter(" ".join(df['Region'].values.tolist()).split(" ")).items())
-    for region in regionList:
-        regionDict[region] = float(regionDict.get(region))/len(df1)
+    for region in regionDict:
+        regionDict[region] = float(regionDict.get(region))/len(df)
+
     if regionDict.get('OOL') >= .24 and regionDict.get('OOL') <= .26:
         oolMetric = True
     else:
@@ -162,13 +43,33 @@ def regionCalc(df):
         regionMetric = False
     return regionMetric
 
+def streamCalc(df):
+    streamDict = dict(Counter(" ".join(delegates['CSR'].values.tolist()).split(" ")).items())
+    CSR_metric = False
+    non_CSR_metric = False
+    FT_metric = False
+    for stream in streamDict:
+        streamDict[stream] = float(streamDict.get(stream))/len(df)
+    #print(streamDict)
+    if streamDict.get('CSR') >= 0.65 and streamDict.get('CSR') <= 0.72:
+        CSR_metric = True
+    if streamDict.get('non-CSR') >= 0.18 and streamDict.get('non-CSR') <= 0.29:
+        non_CSR_metric = True
+    if streamDict.get('FT') >= 0.03 and streamDict.get('FT') <= 0.12:
+        FT_metric = True
+    if CSR_metric == True and non_CSR_metric == True and FT_metric == True and (
+    streamDict.get('CSR') + streamDict.get('non-CSR') + streamDict.get('FT') ==
+    1.0):
+        streamMetric = True
+    else:
+        streamMetric = False
+    return streamMetric
+
 def reindex(df):
     newIndex = range(0,len(df))
     df['Index'] = newIndex
     df = df.set_index('Index')
     return df
-
-regionList = ['OOL', 'London']
 
 applicants = pd.read_csv('test.csv')
 
@@ -176,42 +77,43 @@ rows = random.sample(applicants.index, 800)
 
 delegates = applicants.ix[rows]
 
-
 applicants = applicants.drop(rows)
-
-
-#print (regionDict)
-
-streamDict = dict(Counter(" ".join(delegates['CSR'].values.tolist()).split(" ")).items())
-
-#print (streamDict)
-
-def metricCalc(metricDict):
-    for x in metricDict:
-        metricDict[x] = float(metricDict.get(x))
-    return metricDict
-
-
 
 loop = 0
 dayMetric = False
-for i in range(100):
-    while dayMetric == False:
-        swapFunc(delegates,applicants)
-        dayMetric = dayCalc(delegates)
-        if dayMetric == True:
-            print("Correct number of delegates per day")
+CSR_metric = False
+streamMetric = False
+while dayMetric == False:
+    dataframes = swapFunc(delegates,applicants)
+    delegates = dataframes[0]
+    applicants = dataframes[1]
+    print('This is a day loop')
+    loop +=1
+    print('This is iteration %d' % loop)
+    dayMetric = dayCalc(delegates)
+    if dayMetric == True:
+        print("Correct number of delegates per day")
+        regionMetric = regionCalc(delegates)
+        if regionMetric == False:
+            dataframes = swapFunc(delegates,applicants)
+            delegates = dataframes[0]
+            applicants = dataframes[1]
+            print('Swapping regional candidate')
             regionMetric = regionCalc(delegates)
-            if regionMetric == False:
-                while regionMetric == False:
-                    if loop > 1000:
-                        break
-                    dataframes = swapFunc(delegates,applicants)
-                    delegates = dataframes[0]
-                    applicants = dataframes[1]
-                    print('Swapping candidate')
-                    regionMetric = regionCalc(delegates)
-                    loop +=1
-                    print('This is loop %d' % loop)
-            else:
-                print ("Correct regional ratio overall")
+            loop +=1
+            print('This is loop %d' % loop)
+        else:
+            print('Correct number of delegates per region')
+            streamMetric = streamCalc(delegates)
+            while streamMetric == False:
+                dataframes = swapFunc(delegates,applicants)
+                delegates = dataframes[0]
+                applicants = dataframes[1]
+                print('Swapping stream candidate')
+                streamMetric = streamCalc(delegates)
+                loop +=1
+                print('This is loop %d' % loop)
+                if streamMetric == True:
+                    print("Correct number of delegates per stream")
+                    print("Delegate list compiled")
+                    delegates.to_csv("delegate_test.csv")
